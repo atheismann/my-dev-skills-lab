a92fe405eaebffcd85459748199bc954b0f027f7
Andrew Theismann <andrew@theismannconsulting.com>
2019-07-31 18:26:48 -0500

    added remove method

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 $('#addSkill').on('click', function(evt) {
   $('ul').append($('<li><button class="removeSkill">X</button>' + $('#newSkill').val() + '</li>'
   ))
-})
+});
+
+$('button.removeSkill').on('click', function() {
+  $(this).closest('li').fadeOut(1000, function(){
+    $(this).remove();
+  });
+});
